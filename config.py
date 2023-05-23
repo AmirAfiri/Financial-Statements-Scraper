87cdcb28fb9e8ab2d19ee387c4ea93c30dda2afa
AmirAfiri <91048306+AmirAfiri@users.noreply.github.com>
2023-05-23 14:42:39 -0400

    Create config.py
    Config file for Flask. ADD A SECRET KEY

---
+++ config.py
@@ -0,0 +1,8 @@
+"""Flask App configuration."""
+
+# General Config
+ENVIRONMENT = "development"
+FLASK_APP = "my-app"
+FLASK_DEBUG = True
+SECRET_KEY = ""
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
