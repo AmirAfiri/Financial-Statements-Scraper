7d6b8862d328a0f30afdee883f54dbe50e08ef23
AmirAfiri <91048306+AmirAfiri@users.noreply.github.com>
2023-05-23 14:40:51 -0400

    Update main.py
    More robust version of the previous code with added error handling and validation

--- main.py
+++ main.py
@@ -1,38 +1,77 @@
+from flask import Flask, request, render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from tabula import read_pdf
 import pandas as pd
-import pdfplumber
-from sqlalchemy import create_engine
 import matplotlib.pyplot as plt
+import os
 
-# Function to extract text from pdf
-def extract_text_from_pdf(file):
-    pdf = pdfplumber.open(file)
-    page = pdf.pages[0]
-    text = page.extract_text()
-    pdf.close()
-    return text
+app = Flask(__name__)
+app.config.from_pyfile("config.py")
+db = SQLAlchemy(app)
 
-# Connect to your database
-engine = create_engine('postgresql://username:password@localhost:5432/mydatabase')
+class BalanceSheet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    value = db.Column(db.Float, nullable=False)
 
-# Iterate over your pdf files
-for file in ['file1.pdf', 'file2.pdf', 'file3.pdf']:
-    text = extract_text_from_pdf(file)
-    lines = text.split('\n')
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        
+        file = request.files['file']
+        
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        
+        if not file.filename.lower().endswith('.pdf'):
+            flash('File is not a PDF')
+            return redirect(request.url)
+        
+        try:
+            # Extract tables from PDF using Tabula
+            tables = read_pdf(file, pages='all')
+
+            # Iterate over tables and store in database
+            for table in tables:
+                for index, row in table.iterrows():
+                    if not pd.isnull(row[0]) and not pd.isnull(row[1]):
+                        try:
+                            bs = BalanceSheet(title=row[0], value=float(row[1]))
+                            db.session.add(bs)
+                        except ValueError:
+                            flash('Invalid data in PDF')
+                            return redirect(request.url)
+            db.session.commit()
+        except Exception as e:
+            flash('Error processing PDF')
+            return redirect(request.url)
+
+        flash('File processed successfully')
+        return redirect(url_for('visualization'))
     
-    # Create an empty DataFrame
-    df = pd.DataFrame(columns=['Title', 'Value'])
-    
-    # Logic to extract balance sheet values from the lines and store in DataFrame
-    # This will highly depend on your PDF structure, you will need to customize this
-    for line in lines:
-        if 'balance sheet' in line.lower():
-            title, value = line.split(':')  # assuming each line is "title: value"
-            df = df.append({'Title': title, 'Value': float(value)}, ignore_index=True)
-            
-    # Save DataFrame to SQL
-    df.to_sql('balance_sheet', engine, if_exists='append')
+    return render_template('upload.html')
 
-# Query data from SQL and plot
-df = pd.read_sql_query('SELECT * FROM balance_sheet', engine)
-df.plot(kind='bar', x='Title', y='Value')
-plt.show()
+@app.route('/visualization')
+def visualization():
+    # Query data from database
+    data = BalanceSheet.query.all()
+
+    # Create a DataFrame from the data
+    df = pd.DataFrame([(d.title, d.value) for d in data], columns=['Title', 'Value'])
+
+    # Plot the data
+    plt.figure(figsize=(10,6))
+    plt.bar(df['Title'], df['Value'])
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig('static/plot.png')
+
+    return render_template('visualization.html')
+
+if __name__ == '__main__':
+    db.create_all()  # Create database tables
+    app.run(debug=True)
